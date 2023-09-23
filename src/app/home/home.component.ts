a8e1ed369474858584078f98a5e81322a2c54815
Julianna-Venter <juliannaventer@gmail.com>
2023-09-23 22:57:44 +0200

    merge done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,26 +15,65 @@ export class HomeComponent {
   mySchedule = false;
   modulePage = false;
   tutorPage = false;
+  classPage=false;
   request=true;
+  className = '';
 
   isactivityStream(){
     this.activityStream=true;
     this.myChats = false;
     this.mySchedule = false;
+    this.modulePage=false;
+    this.tutorPage=false;
+    this.classPage=false;
   }
   ismyChats(){
     this.activityStream=false;
     this.myChats = true;
     this.mySchedule = false;
+    this.modulePage=false;
+    this.tutorPage=false;
+    this.classPage=false;
   }
   ismySchedule(){
     this.activityStream=false;
     this.myChats = false;
     this.mySchedule = true;
+    this.modulePage=false;
+    this.tutorPage=false;
+    this.classPage=false;
   }
 
   requestChange(){
     this.request=false;
+  }
+
+  openModule(){
+    this.modulePage=true;
+    this.tutorPage=false;
+    this.classPage=false;
+    this.myChats = false;
+    this.mySchedule = false;
+    this.activityStream=false;
+  }
+
+  openTutor(){
+    this.modulePage=false;
+    this.tutorPage=true;
+    this.classPage=false;
+    this.myChats = false;
+    this.mySchedule = false;
+    this.activityStream=false;
+  }
+
+  openClass(name : string){
+    this.modulePage=false;
+    this.tutorPage=false;
+    this.classPage=true;
+    this.myChats = false;
+    this.mySchedule = false;
+    this.activityStream=false;
+    this.className = name;
   }
 
 }

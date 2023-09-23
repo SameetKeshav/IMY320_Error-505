ea0fa4046d6bddbea37c8878d973ce975461ab98
RoninBrookes <126901461+RoninBrookesTuks@users.noreply.github.com>
2023-09-23 18:48:39 +0200

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  modules=[1,2,3];
+  days=[1,2];
+  notifications = [1,2,3];
+  activityStream=true;
+  myChats = false;
+  mySchedule = false;
+
+  isactivityStream(){
+    this.activityStream=true;
+    this.myChats = false;
+    this.mySchedule = false;
+  }
+  ismyChats(){
+    this.activityStream=false;
+    this.myChats = true;
+    this.mySchedule = false;
+  }
+  ismySchedule(){
+    this.activityStream=false;
+    this.myChats = false;
+    this.mySchedule = true;
+  }
+
 }

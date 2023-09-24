e33f3fa0b9d9baf8b90e2411a77901ab1d97e37b
SameetKeshav <u21479373@tuks.co.za>
2023-09-24 10:51:28 +0200

    Made the schedule arrays

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -20,6 +20,72 @@ export class HomeComponent {
   mySchedule = false;
   days: string[] = [];
   days2: number[] = [];
+  monday = [
+    {
+      module: "COS 333",
+      venue: "IT 4-4",
+      startTime: "09:30",
+      endTime: "10:20",
+      type: "Lecture 1"
+    },
+    {
+      module: "COS 301",
+      venue: "IT 2-26",
+      startTime: "10:30",
+      endTime: "11:20",
+      type: "Lecture 1"
+    }
+  ]
+  tuesday = [
+    {
+      module: "COS 333",
+      venue: "IT 4-4",
+      startTime: "08:30",
+      endTime: "09:20",
+      type: "Lecture 2"
+    },
+    {
+      module: "COS 330",
+      venue: "IT 4-4",
+      startTime: "14:30",
+      endTime: "15:20",
+      type: "Lecture 1"
+    }
+  ]
+  wednesday = [
+    {
+      module: "COS 330",
+      venue: "IT 4-4",
+      startTime: "10:30",
+      endTime: "11:20",
+      type: "Lecture 2"
+    }
+  ]
+  thursday = [
+    {
+      module: "COS 333",
+      venue: "IT 4-4",
+      startTime: "09:30",
+      endTime: "10:20",
+      type: "Practical"
+    },
+    {
+      module: "COS 333",
+      venue: "IT 42-27",
+      startTime: "11:30",
+      endTime: "12:20",
+      type: "Tutorial"
+    }
+  ]
+  friday = [
+    {
+      module: "COS 301",
+      venue: "IT 2-26",
+      startTime: "14:30",
+      endTime: "15:20",
+      type: "Lecture 2"
+    }
+  ]
 
   constructor(){
     this.activityStreamData = activityData;
